c565433ce0de0e638b229f581d4a93c6751304c2
docts2 <90337508+docts2@users.noreply.github.com>
2023-10-21 11:47:18 +0700

    Add files via upload

---
+++ wad_project2/latihan1.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Website Testing</title>
+    </head>
+    <body>
+        <h1>Selamat Datang</h1>
+    </body>
+</html>
---
+++ wad_project2/latihan2.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Website Testing</title>
+    </head>
+    <body>
+        <form method="post" action="testing.php">
+        <label for="angka">Angka:</label>
+        <input type="number" name="angka" id="angka" required>
+
+        <label for="Dropdown">Pilihan:</label>
+        <select name="pilihan" id="pilihan">
+            <option value="1">Opsi 1</option>
+            <option value="2">Opsi 2</option>
+            <option value="3">Opsi 3</option>
+        </select>
+
+        <input type="submit" value="Hitung">
+    </form>
+    </body>
+</html>
